800363ceb3617229738a161449c8e0aa1693ef0f
Jacek Schae <jacek.schae@gmail.com>
2022-05-08 21:27:41 +0200

    15-component-repl

---
+++ increments/15-component-repl/src/dev/user.clj
@@ -0,0 +1,37 @@
+(ns user
+  (:require [io.pedestal.http :as http]
+            [clojure.edn :as edn]
+            [cheffy.routes :as routes]
+            [cheffy.server :as server]
+            [com.stuartsierra.component :as component]))
+
+(defonce system-ref (atom nil))
+
+(defn start-dev []
+  (let [config (-> "src/config/cheffy/development.edn" slurp edn/read-string)]
+    (reset! system-ref
+      (-> config
+          (server/create-system)
+          (component/start))))
+  :started)
+
+(defn stop-dev []
+  (component/stop @system-ref)
+  :stopped)
+
+(defn restart-dev []
+  (stop-dev)
+  (start-dev)
+  :restarted)
+
+(comment
+
+  (:api-server @system-ref)
+
+  (start-dev)
+
+  (restart-dev)
+
+  (stop-dev)
+
+  )
