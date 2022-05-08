bbc9c154cbcc9acb5cca258504ea8fdaa41e903b
Jacek Schae <jacek.schae@gmail.com>
2022-05-08 20:27:18 +0200

    14-component-using

---
+++ increments/14-component-using/src/main/cheffy/routes.clj
@@ -0,0 +1,19 @@
+(ns cheffy.routes
+  (:require [io.pedestal.http.route :as route]))
+
+(defn list-recipes
+  [request]
+  {:status 200
+   :body "list recipes"})
+
+(defn upsert-recipe
+  [request]
+  {:status 200
+   :body "upsert recipes"})
+
+(def routes
+  (route/expand-routes
+    #{{:app-name :cheffy :schema :http :host "learnpedestal.com"}
+      ["/recipes" :get list-recipes :route-name :list-recipes]
+      ["/recipes" :post upsert-recipe :route-name :create-recipe]
+      ["/recipes/:recipe-id" :put upsert-recipe :route-name :update-recipe]}))
